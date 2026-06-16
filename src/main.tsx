01e0c090e335e4059dbd557d9f5efd7c0b821bb0
clearplain <104810318+clearplain@users.noreply.github.com>
2026-06-16 14:50:06 +0800

    Load archive supplemental styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import ArchiveApp from "./ArchiveApp";
 import "./styles.css";
+import "./archive.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
